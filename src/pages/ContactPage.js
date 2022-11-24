ace71c083ecba8b8f82172806c885ff4104e9cc4
Bekah <contact@bekahthe.dev>
2022-11-23 17:34:28 -0700

    moves social links to new file

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm, ValidationError } from '@formspree/react'
+import social from '../res/socials'
 
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName('form')) {
@@ -10,21 +11,36 @@ window.onbeforeunload = () => {
 function ContactPage() {
     const [state, handleSubmit] = useForm('mdojnnel')
     if (state.succeeded) {
-        return <p>Message sent!</p>
+        return <div className='px-8 pt-12 pb-28'>
+            <p className='p-8 bg-bt_dark_blue bg-opacity-30 text-bt_light_blue'>
+                Message sent!
+                <br />
+                <a href='/' className='font-semibold hover:text-bt_pink'>Click here to return to the home page</a> or check out my other social media for more!
+                </p>
+                <div className='social-container flex justify-evenly py-12'>{
+            social.map(socials =>
+                <a href={socials.url} target='blank'>
+                    <img src={socials.img} alt={socials.alt} className='social-icon w-14 md:w-20 hover:animate-spin' />
+                </a>
+            )
+        }
+        </div>
+        </div>
     }
     return (
-        <div className='w-full sm:w-8/12 md:w-7/12 mx-auto'>
-            <div className='mb-6 text-bt_dark_blue font-fira-sans bg-bt_pink py-6 text-center'>
+        <div className='w-full sm:w-8/12 md:w-7/12 mx-auto py-2'>
+            <div className='mb-6 text-bt_dark_blue font-fira-sans bg-bt_pink py-4 text-center'>
             <h2 className='text-2xl md:text-4xl font-semibold'>Looking to reach out?</h2>
             <h3 className=''>Use the form below to send me a message!</h3>
         </div>
+
         <form onSubmit={handleSubmit}>
 
             <input type='text' name='_gotcha' className='hidden' />
 
             <div className='pb-4'>
                 <label htmlFor='name' className='font-fira-code text-bt_light_blue text-md md:text-lg font-semibold'>
-                    Name
+                    Your Name
                 </label>
                 <input id='name' className='w-full p-2' type='name' name='name' required />
                 <ValidationError
@@ -35,7 +51,7 @@ function ContactPage() {
 
             <div className='pb-4'>
                 <label htmlFor='email' className='font-fira-code text-bt_light_blue text-md md:text-lg font-semibold'>
-                    Email
+                    Your Email
                 </label>
                 <input id='email' className='w-full p-2' type='email' name='email' required />
                 <ValidationError
@@ -46,7 +62,7 @@ function ContactPage() {
 
             <div className='pb-4'>
             <label htmlFor='message' className='font-fira-code text-bt_light_blue text-md md:text-lg font-semibold'>
-                    Message
+                    Your Message
                 </label>
                 <textarea id='message' className='w-full p-2 resize-none h-24' name='message' required />
                 <ValidationError
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,50 +1,13 @@
 import React from 'react'
+import social from '../res/socials'
 
 function Homepage() {
-    const social = [
-        {
-            name: 'LinkedIn',
-            url: 'https://linkedin.com/in/rebekahbulych',
-            img: 'https://i.imgur.com/ueJsTr3.png',
-            alt: 'LinkedIn'
-        },
-        {
-            name: 'Github',
-            url: 'https://github.com/panfrieddupa',
-            img: 'https://i.imgur.com/0rWFMI8.png',
-            alt: 'Github'
-        },
-        {
-            name: 'CodePen',
-            url: 'https://codepen.io/panfrieddupa',
-            img: 'https://i.imgur.com/dYzAK28.png',
-            alt: 'CodePen'
-        },
-        {
-            name: 'DeviantArt',
-            url: 'https://deviantart.com/panfrieddupa',
-            img: 'https://i.imgur.com/93WNRgE.png',
-            alt: 'DeviantArt'
-        },
-        {
-            name: 'Twitter',
-            url: 'https://twitter.com/panfrieddupa',
-            img: 'https://i.imgur.com/bwQsnba.png',
-            alt: 'Twitter'
-        },
-        {
-            name: 'Instagram',
-            url: 'https://instagram.com/panfrieddupa',
-            img: 'https://i.imgur.com/VUrcGEI.png',
-            alt: 'Instagram'
-        }
-    ]
 
     return <div className='homepage'>
         <div className='social-container flex justify-evenly pb-6'>{
             social.map(socials =>
                 <a href={socials.url} target='blank'>
-                    <img src={socials.img} alt={socials.alt} className='social-icon spin w-14 md:w-20' />
+                    <img src={socials.img} alt={socials.alt} className='social-icon w-14 md:w-20 hover:animate-spin' />
                 </a>
             )
         }
@@ -52,11 +15,11 @@ function Homepage() {
         <div className='flex'>
             <div className=' text-bt_pink font-fira-sans font-light pt-4 pr-2 text-justify'>
                 <p className='pb-4'>
-                    All icons/logos/graphics were created/recreated by myself
+                    All icons/logos/graphics were created/recreated by myself, and this entire website was coded with NodeJS and ReactJS, with TailwindCSS for styling. Currently hosted on my Github you can check out by clicking the icon below!
                 </p>
                 <p className='pb-4'>
                     The social icons also link to my corresponding profile on that site!
-                    I created them using <a href='https://www.aseprite.org' target='blank' className='font-semibold hover:text-bt_light_blue'>Aseprite</a> which is a small application for creating pixel art.
+                    I created them using <a href='https://www.aseprite.org' target='blank' className='font-medium hover:text-bt_light_blue'>Aseprite</a> which is a small application for creating pixel art.
                 </p>
 
                 <p className='pb-4'>
@@ -72,7 +35,7 @@ function Homepage() {
                 </p>
                 <p className='pb-4'>
                     In my spare time, I enjoy competing in capture-the-flag ethical hacking events, learning new programming languages that aren't a part of my curriculum,
-                    and working on personal projects by navigating them through the software development lifecycle. You can find a graphic version of this information in the <a href="/about" className='font-semibold hover:text-bt_light_blue'>About</a> section.</p>
+                    and working on personal projects by navigating them through the software development lifecycle. You can find a graphic version of this information in the <a href="/about" className='font-medium hover:text-bt_light_blue'>About</a> section.</p>
             </div>
         </div>
 
